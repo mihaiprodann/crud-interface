0725b6cb9cf6f77b6205368c838853bb00215752
mihaiprodann <florin_mihai.prodan@stud.fils.upb.ro>
2024-01-08 15:37:45 +0200

    directory structure

---
+++ pages/add.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Add records to database</title>
+</head>
+<body>
+
+<?php include_once "components/sidebar.php"; ?>
+
+<h1>Add records to database</h1>
+
+<label for="tables">Choose table:</label>
+<select id="tables" name="tables">
+</select>
+
+</body>
+</html>
