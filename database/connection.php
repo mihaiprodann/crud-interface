51d81f70418f2cd44049a453fa2ea1960df00087
mihaiprodann <florin_mihai.prodan@stud.fils.upb.ro>
2024-01-08 19:49:48 +0200

    clean up code

--- database/connection.php
+++ database/connection.php
@@ -7,6 +7,3 @@ $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
-else {
-    echo "Connected successfully";
-}
