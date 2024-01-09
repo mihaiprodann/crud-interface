66e614a12a0846f7f35c0f58c6f3b5ca000f1767
mihaiprodann <florin_mihai.prodan@stud.fils.upb.ro>
2024-01-09 19:49:02 +0200

    resolve sidebar hrefs

--- components/sidebar.php
+++ components/sidebar.php
@@ -15,27 +15,43 @@
 <body>
 <div class="sidebar">
     <div class="logo">
-    <a href="../index.php">
+    <a href="" id="logo_link">
        <header><i class="fa-solid fa-leaf" style="color: #00a3d7;"></i>EcoHarbor</header>
     </a>
     </div>
     <div class="content">
         <ul>
-            <li><a href="pages/add.php"><i class="fa-solid fa-circle-plus"></i>Create Record</a></li>
-            <li><a href="pages/display.php"><i class="fa-solid fa-eye"></i>Display Records</a></li>
-            <li><a href="#"><i class="fa-solid fa-pen-to-square"></i>Update Records</a></li>
-            <li><a href="#"><i class="fa-solid fa-trash"></i>Delete Records</a></li>
-            <li><a href="#"><i class="fa-solid fa-address-card"></i>About</a></li>
+            <li><a href="" id="create_link"><i class="fa-solid fa-circle-plus"></i>Create Record</a></li>
+            <li><a href="" id="display_link"><i class="fa-solid fa-eye"></i>Display Records</a></li>
+            <li><a href="" id="update_link"><i class="fa-solid fa-pen-to-square"></i>Update Records</a></li>
+            <li><a href="" id="delete_link"><i class="fa-solid fa-trash"></i>Delete Records</a></li>
+            <li><a href="" id="about_link"><i class="fa-solid fa-address-card"></i>About</a></li>
         </ul>
     </div>
-        </div>
+</div>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        let url = window.location.href;
+        let create_link = document.getElementById('create_link');
+        let display_link = document.getElementById('display_link');
+        let update_link = document.getElementById('update_link');
+        let delete_link = document.getElementById('delete_link');
+        let logo_link = document.getElementById('logo_link');
 
-
-
-
-
-
+        if (url.includes('pages')) {
+            create_link.href = 'add.php';
+            display_link.href = 'display.php';
+            update_link.href = 'update.php';
+            delete_link.href = 'delete.php';
+            logo_link.href = '../index.php';
+        } else {
+            create_link.href = 'pages/add.php';
+            display_link.href = 'pages/display.php';
+            update_link.href = 'pages/update.php';
+            delete_link.href = 'pages/delete.php';
+            logo_link.href = 'index.php';
+        }
+    }, false);
+</script>
 </body>
 </html>
-
-
